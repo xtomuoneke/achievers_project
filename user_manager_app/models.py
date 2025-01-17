367de854b761f51ef28eac82edb4332e62cad785
xtomuoneke <xtomuoneke@gmail.com>
2025-01-17 16:45:57 +0100

    I created template and static files folder for the user_manager_app. I also created the urls.py file and modified models.py file

--- user_manager_app/models.py
+++ user_manager_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Put the models here
+
 class Profile(models.Model):
     ROLE_CHOICES = [
         ('student', 'Student'),
